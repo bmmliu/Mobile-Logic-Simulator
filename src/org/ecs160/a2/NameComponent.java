1d636794b2dd407adda1c59eabd03f1cd584f92d
bmmliu <bmmliu@ucdavis.edu>
2020-11-28 22:13:21 -0800

    fix font

--- src/org/ecs160/a2/NameComponent.java
+++ src/org/ecs160/a2/NameComponent.java
@@ -2,6 +2,7 @@ package org.ecs160.a2;
 
 
 import com.codename1.ui.Component;
+import com.codename1.ui.Display;
 import com.codename1.ui.Font;
 import com.codename1.ui.Graphics;
 
@@ -10,8 +11,6 @@ public class NameComponent extends Component {
     private int x = 0;
     private int y = 0;
     String name;
-
-    Font smallItalicMonospaceFont = Font.createSystemFont(Font.FACE_MONOSPACE, Font.STYLE_ITALIC, Font.SIZE_SMALL);
 
     public NameComponent(int x, int y, String name) {
         this.x = x;
@@ -39,8 +38,15 @@ public class NameComponent extends Component {
 
     @Override
     public void paint(Graphics g) {
-        g.setFont(smallItalicMonospaceFont);
-        g.setColor(0);
+        g.setColor(0x000000);
+
+        int fontSize = Display.getInstance().convertToPixels(2);
+
+        Font ttfFont = Font.createTrueTypeFont("OpenSans", "OpenSans-Bold.ttf").
+                derive(fontSize, Font.STYLE_PLAIN);
+
+        g.setFont(ttfFont);
+
         g.drawString(name, x, y);
     }
 }
